fa4bdd40a8b599aef5f83a3a644a5067a0e587a3
eswc4 <eswc4@USERKM>
2017-10-08 18:06:26 -0500

    Estructura

--- quizfun/Controllers/HomeController.cs
+++ quizfun/Controllers/HomeController.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace quizfun.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Inicio";
 
             return View();
         }
