47e400d30db9a598baf6754a0337dea10a585810
eswc4 <eswc4@USERKM>
2017-10-08 18:08:59 -0500

    Agregar primeas vistas y controladores

---
+++ quizfun/Controllers/c1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class c1Controller : Controller
+    {
+        // GET: c1
+        public ActionResult c1()
+        {
+            return View();
+        }
+    }
+}
---
+++ quizfun/Controllers/c2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class c2Controller : Controller
+    {
+        // GET: c2
+        public ActionResult c2()
+        {
+            return View();
+        }
+    }
+}
