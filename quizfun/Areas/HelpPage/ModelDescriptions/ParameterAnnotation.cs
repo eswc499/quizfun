775b4b59079ad06171a7b1c26dc9bfea28e26d6d
Edson satoshy Waku Campos <Edson satoshy Waku Campos>
2017-10-08 16:13:00 -0500

    Add project files.

---
+++ quizfun/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace quizfun.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
